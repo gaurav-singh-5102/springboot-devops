7275f3d66368864905ddadd8173bbf01a1534af9
Gaurav Singh <gaurav.singh10@nagarro.com>
2024-04-02 10:08:46 +0530

    Add test for controller

---
+++ src/test/java/com/nagarro/devopsexample/controller/ExampleControllerTests.java
@@ -0,0 +1,27 @@
+package com.nagarro.devopsexample.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.hamcrest.Matchers.equalTo;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ExampleControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testHelloEndpoint() throws Exception {
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello World!")));
+    }
+
+}
