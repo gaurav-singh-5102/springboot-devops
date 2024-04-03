786128fa4d7235c994ec8ee8642fa704f923398d
Gaurav Singh <gaurav.singh10@nagarro.com>
2024-04-03 10:35:22 +0530

    Push event

--- src/main/java/com/nagarro/devopsexample/controller/ExampleController.java
+++ src/main/java/com/nagarro/devopsexample/controller/ExampleController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 public class ExampleController {
-
+    // Hello controller
     @GetMapping("/hello")
     public String hello() {
         return "Hello World!";
